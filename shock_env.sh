cd3ff5d122dc37cadae873b6b7a38270d8c8c251
James <gurtowskij@gmail.com>
2012-08-22 11:14:09 -0400

    bloted out ip

--- shock_env.sh
+++ shock_env.sh
@@ -5,4 +5,4 @@ export SHOCK_USERNAME=james
 export SHOCK_PASSWORD=1234
 
 #http url for shock, including port
-export SHOCK_SERVER=http://140.221.92.74:8000
+export SHOCK_SERVER=http://192.168.0.0:8000
